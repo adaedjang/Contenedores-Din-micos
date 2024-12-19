4a1fba1e7b2f28a9b9a875924993d4565fd4cf39
adaedjang <aedjamay@myuax.com>
2024-12-19 17:39:14 +0100

    .

--- Ejercicio 6/main.cpp
+++ Ejercicio 6/main.cpp
@@ -1,3 +1,16 @@
-//
-// Created by 34601 on 19/12/2024.
-//
+#include <iostream>
+#include <boost/lexical_cast.hpp>
+
+int main() {
+    std::string s = "12345";
+
+    try {
+        int i = boost::lexical_cast<int>(s);
+        std::cout << "Número: " << i << std::endl;
+    } catch (boost::bad_lexical_cast &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
+
